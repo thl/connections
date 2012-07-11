c3d0ca9ca66f69b28b8c3c65c5b3bf5aabbedd19
Heiser Mazariegos <hmazariegos@inside-solutions.net>
2012-07-11 14:39:49 -0600

    updated fathom engine

--- lib/exception_notification.rb
+++ lib/exception_notification.rb
@@ -1,4 +1,5 @@
 ExceptionNotification::Application.config.middleware.use ExceptionNotifier,
     :email_prefix => "[#{APP_NAME} Error] ",
-    :sender_address => "#{APP_NAME} Application Error <do-not-respond@#{APP_DOMAIN}>",
+    #:sender_address => "#{APP_NAME} Application Error <do-not-respond@#{APP_DOMAIN}>",
+    :sender_address => %Q("Application Error" <#{FATHOM_NO_REPLY_ADDRESS}>),
     :exception_recipients => %w(hm5u@virginia.edu thl@collab.itc.virginia.edu)
