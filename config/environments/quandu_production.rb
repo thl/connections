14233e13885d37e0a004e136e8e7e5937690b640
Heiser Mazariegos <hmazariegos@inside-solutions.net>
2012-07-17 05:18:16 -0600

    added mailer setup config

--- config/environments/quandu_production.rb
+++ config/environments/quandu_production.rb
@@ -10,6 +10,16 @@ config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
 #config.action_view.cache_template_loading            = true
 
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => 'inmotionconsulting.net',
+  :user_name            => 'hmazariegos@inmotionconsulting.net',
+  :password             => 'hamf8054',
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  }
+  
 # See everything in the log (default is :info)
 # config.log_level = :debug
 
