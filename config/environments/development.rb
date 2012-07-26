6f3bb7b28092a3f9e200c98a2a5c0cee186d691d
Heiser Mazariegos <hmazariegos@inside-solutions.net>
2012-07-25 21:09:13 -0600

    updated fathom_engine plugin, removed environment file, synced js from thl

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,17 +15,14 @@ FathomConnections::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  #config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :domain               => 'inmotionconsulting.net',
-    :user_name            => 'thl@inmotionconsulting.net',
-    :password             => 'dev1connections',
-    :authentication       => 'plain',
-    :enable_starttls_auto => true  }
-
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :sendmail
+  # Defaults to:
+  # config.action_mailer.sendmail_settings = {
+  #   :location => '/usr/sbin/sendmail',
+  #   :arguments => '-i -t'
+  # }
+  
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
